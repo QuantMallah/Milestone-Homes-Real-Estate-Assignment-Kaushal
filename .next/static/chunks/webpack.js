1512896fa45cbe648134c688c5e7cdb35c854a49
Kaushal Mallah <kmallah21@gmail.com>
2024-04-01 20:49:34 +0530

    Responsive Design

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d17c87833b8b3bbc"; }
+/******/ 		__webpack_require__.h = function() { return "4bafc3adb00b7e38"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -46,76 +46,75 @@ export default function Home({ propertyForRent }) {
           </button>
         </div>
       </header>
-      <hr className='border-1 ' />
-      <div className='px-[10rem] py-[4rem] bg-slate-100'>
-        <div className='flex justify-between items-center'>
-          <div>
-            <h1 className='text-[2rem] font-bold'>Search Properties to Rent</h1>
-          </div>
-          <div>
-            <input className="inline-flex items-center border border-gray-200 font-semibold py-2 px-4 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 mx-2" placeholder='Search with Search Bar' />
-          </div>
-        </div>
-        <div className='flex items-center justify-evenly bg-white my-5 py-3'>
-          <div>
-            <p>Minimum Area (m<sup>2</sup>)</p>
-            <select id="area" value={area} onChange={(e)=>{setArea(e.target.value)}} className='font-bold focus:outline-none'>
-              <option value="60">60</option>
-              <option value="80">80</option>
-              <option value="100">100</option>
-              <option value="120">120</option>
-              <option value="0">All</option>
-            </select>
-          </div>
-          <div>
-            <p>Number of Bathrooms</p>
-            <select id="baths" value={baths} onChange={(e)=>{setBaths(e.target.value)}} className='font-bold focus:outline-none'>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="0">All</option>
-            </select>
-          </div>
-          <div>
-            <p>Price</p>
-            <select id="price" value={price} onChange={(e)=>{setPrice(e.target.value)}} className='font-bold focus:outline-none'>
-              <option value="0-400">Below $400</option>
-              <option value="400-2000">$400-$2000</option>
-              <option value="2000-5000">$2000-$5000</option>
-              <option value="5000-10000">$5000-$10000</option>
-              <option value="10000-15000">$10000-$15000</option>
-              <option value="15000-25000">$15000-$25000</option>
-              <option value="25000-50000">$25000-$50000</option>
-              <option value="0-10000000">All</option>
-            </select>
-          </div>
-          <div>
-            <p>Number of Bedrooms</p>
-            <select id="rooms" value={rooms} onChange={(e)=>{setRooms(e.target.value)}} className='font-bold focus:outline-none'>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="0">All</option>
-            </select>
-          </div>
-          <div>
-            <button className="inline-flex items-center border border-[#eebc1d] bg-[#eebc1d] text-[white] font-semibold py-3 px-5 focus:outline-none rounded text-base mt-4 mx-2 hover:bg-[#e9e9f3] hover:text-[#eebc1d]">Search
-            </button>
-          </div>
-        </div>
-
-        <section className="text-gray-600 body-font">
-          <div className="container py-24 mx-auto">
-            <div className="flex flex-wrap -m-4">
-              {(propertyForRent.filter((e)=>{return e.price>=price.split('-')[0] && e.price<=price.split('-')[1] && (rooms!=0? e.rooms==rooms : e.rooms>=rooms) && (baths!=0? e.baths==baths : e.baths>=baths) && e.area>=area})).map((property) => <Property property={property} key={property.id} />)}
-              {!(propertyForRent.filter((e)=>{return e.price>=price.split('-')[0] && e.price<=price.split('-')[1] && (rooms? e.rooms==rooms : e.rooms>=rooms) && (baths!=0? e.baths==baths : e.baths>=baths) && e.area>=area})).length && <div className='text-[3rem] font-bold w-[100%] text-center'>No results found for this filter</div>}
-            </div>
-          </div>
-          <Contact />
-        </section>
+  <hr className='border-1 ' />
+  <div className='px-4 md:px-[10rem] py-4 md:py-[4rem] bg-slate-100'>
+  <div className='flex flex-col md:flex-row items-center justify-between'>
+    <div className='mb-4 md:mb-0'>
+      <h1 className='text-2xl md:text-[2rem] font-bold'>Search Properties to Rent</h1>
+    </div>
+    <div>
+      <input className="block md:inline-block w-full md:w-auto border border-gray-200 font-semibold py-2 px-4 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0" placeholder='Search with Search Bar' />
+    </div>
+  </div>
+  <div className='flex flex-col md:flex-row items-center justify-between bg-white my-5 py-3'>
+    <div className="w-full md:w-auto mb-4 md:mb-0 md:mr-4">
+      <p>Minimum Area (m<sup>2</sup>)</p>
+      <select id="area" value={area} onChange={(e)=>{setArea(e.target.value)}} className='font-bold focus:outline-none w-full'>
+        <option value="60">60</option>
+        <option value="80">80</option>
+        <option value="100">100</option>
+        <option value="120">120</option>
+        <option value="0">All</option>
+      </select>
+    </div>
+    <div className="w-full md:w-auto mb-4 md:mb-0 md:mr-4">
+      <p>Number of Bathrooms</p>
+      <select id="baths" value={baths} onChange={(e)=>{setBaths(e.target.value)}} className='font-bold focus:outline-none w-full'>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="0">All</option>
+      </select>
+    </div>
+    <div className="w-full md:w-auto mb-4 md:mb-0 md:mr-4">
+      <p>Price</p>
+      <select id="price" value={price} onChange={(e)=>{setPrice(e.target.value)}} className='font-bold focus:outline-none w-full'>
+        <option value="0-400">Below $400</option>
+        <option value="400-2000">$400-$2000</option>
+        <option value="2000-5000">$2000-$5000</option>
+        <option value="5000-10000">$5000-$10000</option>
+        <option value="10000-15000">$10000-$15000</option>
+        <option value="15000-25000">$15000-$25000</option>
+        <option value="25000-50000">$25000-$50000</option>
+        <option value="0-10000000">All</option>
+      </select>
+    </div>
+    <div className="w-full md:w-auto mb-4 md:mb-0 md:mr-4">
+      <p>Number of Bedrooms</p>
+      <select id="rooms" value={rooms} onChange={(e)=>{setRooms(e.target.value)}} className='font-bold focus:outline-none w-full'>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="0">All</option>
+      </select>
+    </div>
+    <div>
+      <button className="inline-block w-full md:w-auto border border-[#eebc1d] bg-[#eebc1d] text-[white] font-semibold py-3 px-5 focus:outline-none rounded text-base mt-4 md:mt-0 hover:bg-[#e9e9f3] hover:text-[#eebc1d]">Search</button>
+    </div>
+  </div>
+  <section className="text-gray-600 body-font">
+    <div className="container py-8 md:py-24 mx-auto">
+      <div className="flex flex-wrap -m-2">
+        {(propertyForRent.filter((e)=>{return e.price>=price.split('-')[0] && e.price<=price.split('-')[1] && (rooms!=0? e.rooms==rooms : e.rooms>=rooms) && (baths!=0? e.baths==baths : e.baths>=baths) && e.area>=area})).map((property) => <Property property={property} key={property.id} />)}
+        {!(propertyForRent.filter((e)=>{return e.price>=price.split('-')[0] && e.price<=price.split('-')[1] && (rooms? e.rooms==rooms : e.rooms>=rooms) && (baths!=0? e.baths==baths : e.baths>=baths) && e.area>=area})).length && <div className='text-[1.5rem] md:text-[3rem] font-bold w-full text-center'>No results found for this filter</div>}
       </div>
+    </div>
+    <Contact />
+  </section>
+</div>
       
-    </>
+</>
+
   )
 }
 
